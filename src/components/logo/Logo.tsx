af2d650614eaff6cbf67bed24ebfa5c56de2ce4d
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-04-02 16:09:47 +0300

    feat: add header

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+export const Logo = () => {
+    return (
+        <a href="#">
+            <Icon iconId={"dockerSvg"} width={"50"} height={"50"}/>
+        </a>
+    );
+};
