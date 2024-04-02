9de73e62b7527654b33df67bb5131e348085e576
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-04-02 20:15:34 +0300

    feat: add main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import photo from '../../../assets/images/photo.jpg'
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
-const Main = () => {
+export const Main = () => {
     return (
-        <div>
-            
-        </div>
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-between"}>
+                <div>
+                    <span>Hi 👋,</span>
+                    <span>My name is</span>
+                    <Name>Dmitriy Kutsilin</Name>
+                    <MainTitle>I am Web Developer</MainTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
 
-export default Main;
+const StyledMain = styled.div`
+    min-height: 630px;
+    background-color: #fff;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 350px;
+    object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+
+`
+
+const Name = styled.h2`
+
+`
